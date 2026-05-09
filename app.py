d605f78b0fc468cc869062983964e118de5eb5b3
Negarhtf <hatef.negar@gmail.com>
2026-05-09 14:56:52 +0200

    Add country filter dropdown

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 import plotly.express as px
 
@@ -7,12 +7,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    df = pd.read_csv("soil_data.csv")
+    original_df = pd.read_csv("soil_data.csv")
+
+    selected_country = request.args.get("country")
+
+    if selected_country:
+        df = original_df[original_df["country"] == selected_country]
+    else:
+        df = original_df
+
+    countries = sorted(original_df["country"].unique())
 
     sample_count = len(df)
     avg_moisture = df["moisture"].mean()
 
-    fig = px.scatter(
+    scatter_fig = px.scatter(
         df,
         x="temperature",
         y="moisture",
@@ -20,6 +29,9 @@ def home():
         hover_data=["location", "country", "ph", "organic_carbon"],
         title="Temperature vs Moisture"
     )
+
+    chart = scatter_fig.to_html(full_html=False)
+
     avg_by_country = (
         df.groupby("country", as_index=False)["moisture"]
         .mean()
@@ -35,17 +47,17 @@ def home():
 
     bar_chart = bar_fig.to_html(full_html=False)
 
-    chart = fig.to_html(full_html=False)
-
     table = df.head(10).to_html(index=False)
 
     return render_template(
         "index.html",
         sample_count=sample_count,
-        avg_moisture=avg_moisture,
+        avg_moisture=round(avg_moisture, 2),
         chart=chart,
         bar_chart=bar_chart,
-        table=table
+        table=table,
+        countries=countries,
+        selected_country=selected_country
     )
 
 
