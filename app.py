424205e1b7f22a764a4568ddb3e40256a8116077
Negarhtf <hatef.negar@gmail.com>
2026-05-09 12:59:17 +0200

    Add data preview table

--- app.py
+++ app.py
@@ -7,37 +7,30 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-
     df = pd.read_csv("soil_data.csv")
 
     sample_count = len(df)
-
-    avg_moisture = (
-        df["moisture"]
-        .mean()
-    )
+    avg_moisture = df["moisture"].mean()
 
     fig = px.scatter(
         df,
         x="temperature",
         y="moisture",
         color="soil_type",
-        hover_data=[
-            "location",
-            "country"
-        ],
+        hover_data=["location", "country", "ph", "organic_carbon"],
         title="Temperature vs Moisture"
     )
 
-    chart = fig.to_html(
-        full_html=False
-    )
+    chart = fig.to_html(full_html=False)
+
+    table = df.head(10).to_html(index=False)
 
     return render_template(
         "index.html",
         sample_count=sample_count,
         avg_moisture=avg_moisture,
-        chart=chart
+        chart=chart,
+        table=table
     )
 
 
